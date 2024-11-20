6bd6aa8f757184bea4f78f16b1201e6850e51e5e
earayu <earayu@163.com>
2024-11-20 16:23:12 +0800

    update

--- pkg/plugin.go
+++ pkg/plugin.go
@@ -1,8 +1,8 @@
 package pkg
 
 import (
+	_ "github.com/wesql/wescale-wasm-plugin-sdk/pkg/guest_functions"
 	"github.com/wesql/wescale-wasm-plugin-sdk/pkg/types"
-	_ "github.com/wesql/wescale-wasm-plugin-sdk/pkg/v1alpha2/guest_functions"
 )
 
 func InitWasmPlugin(plugin types.WasmPlugin) {
